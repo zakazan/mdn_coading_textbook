9eacc05229f4b8a39753b9787f388aff11d090f3
zakazan <hinakofumo1226@gmail.com>
2022-05-05 21:43:21 +0900

    SPナビゲーション

--- EXERCISE01/js/hamburger.js
+++ EXERCISE01/js/hamburger.js
@@ -1,5 +1,7 @@
 const hamburgerButton = document.getElementById('js-btnHamburger');
+const navigation = document.getElementById('js-navigationWrapper');
 
 hamburgerButton.addEventListener('click', () => {
     hamburgerButton.classList.toggle('hamburger--open');
+    navigation.classList.toggle('navigation--open');
 });
