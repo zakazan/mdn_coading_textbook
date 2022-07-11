077817e81916e7605e88b1a191bdf6e08a07acd8
zakazan <hinakofumo1226@gmail.com>
2022-07-11 09:19:19 +0900

    ディレクトリ構成の整理

--- EXERCISE02/js/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburgerButton = document.getElementById('js-btnHamburger');
-const navigation = document.getElementById('js-navigationWrapper');
-
-hamburgerButton.addEventListener('click', () => {
-    hamburgerButton.classList.toggle('hamburger--open');
-    navigation.classList.toggle('navigation--open');
-});
